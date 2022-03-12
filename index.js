d263c0924b96cdb669974f50c30c420861258cef
Abdelali12-codes <jadelmoulaa3@gmail.com>
2022-03-12 16:20:06 +0100

    sync and waitfortoken task

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+var fun = async (value) =>{
+    
+    return value
+}
+
+var resp = fun('Hello Abdelali').then((value)=>{
+    return "abdelali jadelmoua"
+})
+resp.then((value)=>{
+    console.log(value)
+})
+
+console.log('Abdelali')
